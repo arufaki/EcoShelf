0e70ddb6258a00032cda6d7569621b56fcc42147
arufaki <fakihkazorla@gmail.com>
2024-11-10 22:17:33 +0700

    update: route link books

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import BookDetailPage from "../pages/BookDetailPage";
+import BooksPage from "../pages/BooksPage";
 
 const AppRouter = () => {
     return (
@@ -13,6 +14,7 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/detail" element={<BookDetailPage />} />
+                <Route path="/books" element={<BooksPage />} />
             </Routes>
         </Router>
     );
