cabbf41fbaf4f7e0e9532185cf1463c7a02f8b57
arufaki <fakihkazorla@gmail.com>
2024-11-10 20:33:17 +0700

    add: book details page

---
+++ src/pages/BookDetailPage.jsx
@@ -0,0 +1,23 @@
+import Header from "../components/organisms/Reusable/Header";
+import Footer from "../components/organisms/Reusable/Footer";
+import Main from "../layouts/Main";
+import { useAppContext } from "../context/AppContext";
+import BookInfo from "../components/organisms/BookDetail/BookInfo";
+import BookSynopsis from "../components/organisms/BookDetail/BookSynopsis";
+import BookReviews from "../components/organisms/BookDetail/BookReviews";
+
+const BookDetailPage = () => {
+    return (
+        <>
+            <Header />
+            <Main className="max-w-[1440px] mx-auto">
+                <BookInfo />
+                <BookSynopsis />
+                <BookReviews />
+            </Main>
+            <Footer />
+        </>
+    );
+};
+
+export default BookDetailPage;
