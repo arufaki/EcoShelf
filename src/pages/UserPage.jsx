8d4796d34a1e3235c58de263ade956dd798f843b
arufaki <fakihkazorla@gmail.com>
2024-11-14 17:45:48 +0700

    feat(user-page): integrate wishlist component

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,9 @@
 import BookReviews from "../components/organisms/BookDetail/BookReviews";
 import UserProfile from "../components/organisms/ProfileUser/UserProfile";
-import WishlistCard from "../components/organisms/ProfileUser/WishlistCard";
 import Footer from "../components/organisms/Reusable/Footer";
 import Header from "../components/organisms/Reusable/Header";
 import Main from "../layouts/Main";
+import Wishlist from "../components/organisms/ProfileUser/Wishlist";
 
 const UserPage = () => {
     return (
@@ -11,7 +11,7 @@ const UserPage = () => {
             <Header />
             <Main className="max-w-[1440px] mx-auto">
                 <UserProfile />
-                <WishlistCard />
+                <Wishlist />
                 <BookReviews title="Review History" userProfile={true} />
             </Main>
             <Footer />
