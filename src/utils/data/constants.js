803d6caacc5ba184da0a1f7e602a39327d392021
arufaki <fakihkazorla@gmail.com>
2024-11-15 14:41:53 +0700

    fix(api): add google book api key

--- src/utils/data/constants.js
+++ src/utils/data/constants.js
@@ -1,4 +1,5 @@
-const API_URL =
-    "https://www.googleapis.com/books/v1/volumes?q=sustainability+OR+climate+change+OR+renewable+energy+OR+conservation+OR+environmental+protection+OR+ecology+OR+biodiversity+OR+carbon+footprint+OR+green+technology+OR+reforestation+OR+circular+economy+OR+zero+waste+OR+pollution+control+OR+wildlife+conservation+OR+green+energy&maxResults=40";
+const googleBookKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const API_URL = `https://www.googleapis.com/books/v1/volumes?q=sustainability+OR+climate+change+OR+renewable+energy+OR+conservation+OR+environmental+protection+OR+ecology+OR+biodiversity+OR+carbon+footprint+OR+green+technology+OR+reforestation+OR+circular+economy+OR+zero+waste+OR+pollution+control+OR+wildlife+conservation+OR+green+energy&maxResults=40&key=${googleBookKey}`;
 
 export default API_URL;
