cf229f502586b69e584c83c2cdf3a3a636594789
arufaki <fakihkazorla@gmail.com>
2024-11-17 02:36:09 +0700

    refactor(auth): moves toast to separate file & adds logout alert

--- src/utils/function/supabase/signOutUser.js
+++
@@ -1,8 +0,0 @@
-import { supabase } from "../../../api/supabaseClient";
-
-const signOutUser = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) throw error;
-};
-
-export default signOutUser;
