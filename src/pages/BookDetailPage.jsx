19a54f2402eeb919b88abbe1fce52b3938e8c342
arufaki <fakihkazorla@gmail.com>
2024-11-17 00:33:08 +0700

    fix(bookinfo): fixes book detail page routing issue

--- src/pages/BookDetailPage.jsx
+++ src/pages/BookDetailPage.jsx
@@ -4,18 +4,28 @@ import Main from "../layouts/Main";
 import BookInfo from "../components/organisms/BookDetail/BookInfo";
 import BookSynopsis from "../components/organisms/BookDetail/BookSynopsis";
 import BookReviews from "../components/organisms/BookDetail/BookReviews";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
 
 const BookDetailPage = () => {
     const location = useLocation();
-    const book = location.state;
+    const bookState = location.state;
+    const { books } = useAppContext();
+    const { id } = useParams();
+    const book = bookState || books.find((b) => b.id == id);
     return (
         <>
             <Header />
             <Main className="max-w-[1440px] mx-auto">
-                <BookInfo book={book} />
-                <BookSynopsis book={book} />
-                <BookReviews title="Reader Reviews" userProfile={false} book={book} />
+                {!book ? (
+                    <p>Not Found Book</p>
+                ) : (
+                    <>
+                        <BookInfo book={book} />
+                        <BookSynopsis book={book} />
+                        <BookReviews title="Reader Reviews" userProfile={false} book={book} />
+                    </>
+                )}
             </Main>
             <Footer />
         </>
